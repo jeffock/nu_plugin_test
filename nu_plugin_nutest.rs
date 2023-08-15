3619f8360eb66a440caad57fc7e3dc01d524b7c7
jeffock <jeffock1656@gmail.com>
2023-08-15 17:44:40 -0400

    initial commit

---
+++ nu_plugin_nutest.rs
@@ -0,0 +1,28 @@
+use std::process::Command;
+
+fn main() {
+    let nushell_command = r#"echo 'Hello from NuShell!'"#;
+
+    let output = Command::new("nu")
+        .arg("-c")
+        .arg(nushell_command)
+        .output();
+
+    match output {
+        Ok(output_data) => {
+            if output_data.status.success() {
+                let stdout = String::from_utf8_lossy(&output_data.stdout);
+                let stderr = String::from_utf8_lossy(&output_data.stderr);
+                println!("Command executed successfully. Output:");
+                println!("Stdout:\n{}", stdout);
+                println!("Stderr:\n{}", stderr);
+            } else {
+                println!("Command failed to execute.");
+            }
+        }
+        Err(e) => {
+            println!("Error executing command: {}", e);
+        }
+    }
+}
+
